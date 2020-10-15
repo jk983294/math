461d7fa80309d4d1b8db9e0bb557ea7e36189d35
jk983294 <jk983294@gmail.com>
2020-10-15 23:00:46 +0800

    ggplot

--- r/library/ggplot2/qplot/line.r
+++ r/library/ggplot2/qplot/line.r
@@ -1,7 +1,21 @@
-library("ggplot2")
+library(ggplot2)
+library(data.table)
+library(dplyr)
 
 # dot graph
 x <- c(-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)
 y <- x^3
+y1 <- x^2
+dt <- data.table(x, y, y1)
+
 qplot(x, y)
 qplot(x, y, geom = "line")
+
+# multiple line method1
+ggplot(dt, aes(x)) +
+    geom_line(aes(y = y, colour = "y")) +
+    geom_line(aes(y = y1, colour = "y1"))
+
+# multiple line method2
+melted = melt(dt, id.vars="x")
+ggplot(data=melted, aes(x=x, y=value, group=variable, colour=variable)) + geom_line()
