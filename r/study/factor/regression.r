33c5723fd322aa698f82c5f3eb35ad6c68979298
jk983294 <jk983294@gmail.com>
2020-10-16 18:10:48 +0800

    ridge

--- r/study/factor/regression.r
+++ r/study/factor/regression.r
@@ -10,6 +10,7 @@ R2 <- function(pred, obs, formula = "corr", na.rm = FALSE) {
 }
 
 split <- 0.8
+#dt1 <- read.csv("~/github/barn/train/factor.csv")
 dt1 <- read.csv("~/github/MyTmp/sim/cmake-build-debug/pmfut/factor.csv")
 plot(density(dt1$ret), main = "ret")
 qqnorm(dt1$ret)
@@ -33,40 +34,6 @@ pred_train <- predict(fit, newdata = train)
 R2(pred_train, train$ret)
 cor(cbind(pred, validation$ret))
 cor(cbind(pred_train, train$ret))
-
-# ridge regression
-y <- train$ret
-x <- train %>% select(-ret) %>% data.matrix()
-y_test <- validation$ret
-x_test <- validation %>% select(-ret) %>% data.matrix()
-n.coef <- 100
-lambdas <- 10^seq(-4, 3, length = n.coef)
-
-fit <- glmnet(x, y, intercept = FALSE, alpha = 0, lambda = lambdas)
-summary(fit)
-coef.mat <- coef(fit)[-1, ]
-oos.mat <- rep(0, n.coef)
-for (i in 1:n.coef) {
-    cur.coef <- coef.mat[, i]
-    pred <- x_test %*% cur.coef
-    oos.mat[i] <- R2(pred, y_test, "traditional")
-}
-plot(oos.mat, ylab = "R2", main = "ridge")
-
-best <- which.max(oos.mat)
-best
-oos.mat[best]
-
-# ridge regression cross validation
-y <- dt1 %>% select(ret) %>% scale(center = TRUE, scale = FALSE) %>% data.matrix()
-X <- dt1 %>% select(-ret) %>% data.matrix()
-fit <- cv.glmnet(X, y, intercept = FALSE, standardize=TRUE, alpha = 0, lambda = lambdas, nfolds = 10)
-plot(fit)
-lambda_best <- fit$lambda.min
-model_cv <- glmnet(X, y, intercept = FALSE, standardize = TRUE, alpha = 0, lambda = lambda_best)
-y_hat_cv <- predict(model_cv, X)
-R2(y_hat_cv, y)
-cor(cbind(y_hat_cv, y))
 
 # lasso
 n.coef <- 100
