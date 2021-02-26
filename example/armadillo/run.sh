2b98c4d5ed9008c81437e60c99346b447d534c70
jk983294 <jk983294@gmail.com>
2021-02-26 18:11:30 +0800

    mkl

--- example/armadillo/run.sh
+++ example/armadillo/run.sh
@@ -5,3 +5,10 @@ cd armadillo-9.900.1/
 cmake -D DETECT_HDF5=false .
 make
 sudo make install
+
+# compile with mkl
+cpu-info                                # check if intel cpu
+sh l_onemkl_p_2021.1.1.52_offline.sh
+ln -s /opt/intel/oneapi/mkl/2021.1.1 /opt/intel/mkl
+vim /etc/ld.so.conf.d/libmkl.conf  # add /opt/intel/mkl/lib/intel64/
+ldconfig
