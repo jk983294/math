669a7f679156397b84d35e5146da7145a5c6b01f
jk983294 <jk983294@gmail.com>
2021-03-16 22:29:39 +0800

    tweak

--- r/library/ggplot2/type/histogram.r
+++ r/library/ggplot2/type/histogram.r
@@ -1,3 +1,7 @@
 library(ggplot2)
+
 data(singer, package = "lattice")
 ggplot(singer, aes(x = height)) + geom_histogram()
+
+scores <- 1:10
+qplot(scores, geom = "histogram")
--- r/library/table/aggregate.r
+++ r/library/table/aggregate.r
@@ -1,6 +1,7 @@
 library(data.table)
+library(moments)
 
-dt <- data.table(x = c("a", "b", "a", "b"), y = c(2., 1., 3., 4.), z = c(1., 3., 2., 2.))
+dt <- data.table(x = c("a", "b", "a", "b"), y = c(2., 1., 3., NA), z = c(1., 3., NA, 2.))
 
 ans <- dt[, sum((y + z) < 5)]  # how many rows which (y + z) < 5
 ans
@@ -12,7 +13,7 @@ ans <- dt[x == 'b', .N]  # how many rows which x == 'b' using special symbol .N
 ans
 
 # mean of selected rows
-ans <- dt[x == 'a', .(m_y = mean(y), m_z = mean(z))]  
+ans <- dt[x == 'a', .(m_y = mean(y), m_z = mean(z))]
 ans
 
 # group
@@ -29,7 +30,21 @@ ans
 dt[, print(.SD), by = x]  # .SD contains all the columns except the grouping columns by default
 dt[, head(.SD, 1), by = x]  # first row for each x
 # group function apply to all columns
-dt[, lapply(.SD, mean), by = x]  
+dt[, lapply(.SD, mean), by = x]
+
+dt_stats <- function(x) {
+  mm <- colMeans(x, na.rm = TRUE)
+  ss <- sapply(x, sd, na.rm = TRUE)
+  median_ <- sapply(x, median, na.rm = TRUE)
+  min_ <- sapply(x, min, na.rm = TRUE)
+  max_ <- sapply(x, max, na.rm = TRUE)
+  skew_ <- sapply(x, skewness, na.rm = TRUE)
+  kurtosis_ <- sapply(x, kurtosis, na.rm = TRUE)
+  list(names = names(x), median = median_, mean = mm, sd = ss, min = min_, max = max_, skew = skew_, kurtosis = kurtosis_)
+}
+
+dt[, dt_stats(.SD), by = x]                 # group stats
+dt[, dt_stats(.SD), .SDcols = c("y", "z")]  # whole column stats
 
 # expression in by
 ans <- dt[, .(.N), by = .(y > 1.5)]   # each group count
