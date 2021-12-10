2075e7868e6753c19e25ac2307289e6625c5277c
jk983294 <jk983294@gmail.com>
2021-12-10 17:12:04 +0800

    add stat

--- include/math_stats_hft.h
+++ include/math_stats_hft.h
@@ -38,6 +38,87 @@ std::pair<double, double> hft_math_sign_ratio(const std::vector<std::vector<T>>&
     return {NAN, NAN};
 }
 
+template <typename T>
+vector<double> hft_acf(const std::vector<std::vector<T>>& data_, int n_bar) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::acf(data_[ii], n_bar);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_skew(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::math_skew(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_kurtosis(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::math_kurtosis(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_quantile(const std::vector<std::vector<T>>& data_, double q) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::quantile_const(data_[ii], q);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_median(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::median(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_mean(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::mean(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_sd(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::std(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_max(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::v_max(data_[ii]);
+    }
+    return ret;
+}
+
+template <typename T>
+vector<double> hft_min(const std::vector<std::vector<T>>& data_) {
+    vector<double> ret(data_.size(), NAN);
+    for (size_t ii = 0; ii < data_.size(); ++ii) {
+        ret[ii] = ornate::v_min(data_[ii]);
+    }
+    return ret;
+}
+
 }  // namespace ornate
 
 #endif
