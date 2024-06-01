1501ddcc7fb4eb5ef0d80f916a6e79d2d9b85624
jk983294 <jk983294@gmail.com>
2024-06-01 14:21:58 +0800

    sym

---
+++ r/mostly/prob/counting.r
@@ -0,0 +1,22 @@
+A <- function(n, k) {
+    factorial(n) / factorial(n - k)
+}
+C <- function(n, k) {
+    choose(n, k)
+}
+
+factorial(9) # 9!
+choose(9, 2) # C(n, k)
+C(9, 2) # C(n, k)
+A(9, 2) # A(n, k)
+
+# x_1 + ... + x_r = n 的非负整数解的个数 C(n + r - 1, r - 1)
+n <- 4
+r <- 2
+(ans <- choose(n + r - 1, r - 1))
+
+
+combn(4, 2) # list all combination of C(n, k)
+combinat::combn(4, 2) # list all combination of C(n, k)
+combinat::permn(3) # list all combination of n!
+
